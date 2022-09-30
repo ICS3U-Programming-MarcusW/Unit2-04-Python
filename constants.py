8fa62d523e6618abb10ccb9ccf97b3308cd7eeb2
Marcus Wehbi <marcus.wehbi@stu.ocsb.ca>
2022-09-30 02:35:48 +0000

    Program to calculate the cost of a pizza with a given diameter

---
+++ constants.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# Created by: Marcus Wehbi
+# Created on: Sep 29th, 2022
+# This program contains constants.
+HST = 0.13
+LABOUR_COST = 2.00
+RENTAL_COST = 2.25
+INGRED_COST = 1.5
